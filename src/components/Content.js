f84a2924cd4c84f739638a5f85c101cfbb9603cd
TinkerSpace <tinkerspace@TinkerSpaces-Mac-mini.local>
2024-03-27 20:11:25 +0530

    almost completed

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,53 +1,53 @@
 import React from "react";
 
 const Content = () => {
+
+  const emailAddress = "alexmathai07@gmail.com"
+  const handleDownload = () => {
+    const element = document.createElement("a");
+    element.setAttribute("href", "https://drive.google.com/file/d/1nWM5K59UkHFvq1qUxodhf3FWfWB_xUO1/view?usp=drive_link");
+    element.setAttribute("download", "Alex_Mathai_Resume.pdf");
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
   return (
     <div className="h-screen flex flex-col w-full bg-slate-600 text-white">
       <div className="flex flex-grow"></div>
       <div className="flex justify-center">
         <div>
-          <p className="text-3xl">👋 I'm Alex Mathai</p>
+          <p className="text-3xl md:text-5xl md:my-2 md:mb-10">👋 I'm Alex mathai</p>
 
           {/* links */}
-          <div className="mt-5 flex justify-evenly">
+          <div className="mt-5 flex items-center justify-evenly">
             {/* github */}
-            <a href="">
-              <img className="w-8" src="./img/github.png" alt="github"></img>
+            <a href="https://github.com/Alexmathai2001">
+            <i class="bi bi-github text-4xl"></i>
             </a>
             {/* linkedin */}
-            <a href="">
-              <img
-                className="w-8"
-                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
-              ></img>
+            <a href="https://www.linkedin.com/in/alex-mathai/">
+            <i class="bi bi-linkedin text-4xl"></i>
             </a>
             {/* telegram */}
-            <a href="">
-              <img
-                className="w-8"
-                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
-              ></img>
-            </a>
-            {/* mail */}
-            <a href="">
-              <img
-                className="w-8"
-                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
-              ></img>
+            <a href="https://t.me/a1exmathai">
+            <i class="bi bi-telegram text-4xl"></i>
             </a>
             {/* insta */}
-            <a href="">
-              <img
-                className="w-8"
-                src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
-              ></img>
+            <a href="https://www.instagram.com/a1exmathai/">
+            <i class="bi bi-instagram text-4xl"></i>
             </a>
           </div>
 
           {/* buttons */}
-          <div className="mt-3 flex justify-evenly">
-            <button className="px-5 py-2 bg-black rounded-lg text-sm">Resume</button>
-            <button className="px-5 py-2 bg-black rounded-lg text-sm">Email Me</button>
+          <div className="mt-3 flex justify-evenly md:mt-12">
+            <button onClick={handleDownload} className="px-5 md:px-8 md:py-4 py-2 bg-black rounded-lg text-sm">Resume</button>
+            <a
+          href={`mailto:${emailAddress}`}
+          className="px-5 md:px-8 md:py-4 py-2 bg-black rounded-lg text-sm block text-center"
+          style={{ textDecoration: 'none' }}
+        >
+          Email Me
+        </a>
           </div>
         </div>
       </div>
--- src/components/ProjectContent.js
+++ src/components/ProjectContent.js
@@ -22,14 +22,13 @@ const ProjectContent = () => {
     <div className="h-auto min-h-screen flex flex-col w-full bg-slate-600 text-white">
       <div className="flex flex-grow"></div>
       <div className="flex justify-center mt-36 mb-20">
-        <div className="">
+        <div className="flex flex-col items-center">
           <p className="capitalize text-center mb-5">my github Streaks</p>
 
-          
           {/* github api */}
-          <div className="">
+          <div className="flex justify-center w-full ">
             <img
-            className="w-60"
+              className="w-60 md:w-[30rem]"
               src="https://github-readme-streak-stats.herokuapp.com/?user=alexmathai2001&theme=dark"
               alt="jigar-sable"
             />
@@ -38,25 +37,28 @@ const ProjectContent = () => {
           <div className="flex flex-col items-center">
             <p className="text-center my-5">Project</p>
             {/* card container */}
-            <div className="grid grid-cols-1 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
               {/* card */}
               {repoData?.map((item, index) => (
-                <div className="w-52 h-auto rounded-lg bg-black  p-5">
+                <div className="w-52 h-[20rem] flex flex-col items-center rounded-lg bg-black  p-5">
                   <div className="flex flex-col justify-center items-center">
                     <p className=" font-bold my-2 capitalize">{item?.name}</p>
                     <p className="text-xs my-2 text-center text-gray-300 capitalize">
                       {item?.description || "descritpion not added by the user"}
                     </p>
-                    <p className="text-xs">Major Language : {item?.language}</p>
+                    
                   </div>
-
-                  <a
-                    href={item?.html_url}
-                    className="text-white flex justify-center items-center mt-4 bg-gray-600 j w-3/6 rounded-xl"
-                  >
-                    <i class="bi bi-code"></i>
-                    <p className="text-sm font-light">code</p>
-                  </a>
+                  <div className="flex flex-grow"></div>
+				  <p className="text-xs my-3">Major Language : {item?.language}</p>
+                  
+                    <a
+                      href={item?.html_url}
+                      className="text-white flex justify-center items-center mt-4 bg-gray-600 j w-3/6 rounded-xl "
+                    >
+                      <i class="bi bi-code"></i>
+                      <p className="text-sm font-light">code</p>
+                    </a>
+                  
                 </div>
               ))}
             </div>
